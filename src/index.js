8a98f2fa269e9b82ba3598a79671bcbbc0d79e6c
Justin Seitz <justinseitz2016@gmail.com>
2021-06-23 11:47:41 -0700

    bootstrap components can be added successfully

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(props) {
     return (
+        <>
         <h1>Hello world</h1>
+        <Button>Test</Button>
+        <Toast>Hello world</Toast>
+        </>
     );
 }
 
