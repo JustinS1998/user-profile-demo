82f556a600834f7c7135c56b63a9b3bf54f793a7
Justin Seitz <justinseitz2016@gmail.com>
2021-06-23 12:42:48 -0700

    fetches user on page looad

--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -2,9 +2,12 @@ import React, {useEffect} from 'react';
 import Container from 'react-bootstrap/Container';
 
 export function UserInfo(props) {
+    const {getRandomUser} = props;
+
     useEffect(() => {
         console.log('page loaded');
-    }, []);
+        getRandomUser();
+    }, [getRandomUser]);
 
     return (
         <Container>UserInfo</Container>
--- src/UserInfo/UserInfoContainer.js
+++ src/UserInfo/UserInfoContainer.js
@@ -2,13 +2,12 @@ import React, { useState } from 'react';
 import { UserInfo } from './UserInfo';
 
 export function UserInfoContainer(props) {
-    const [user, setUser] = useState({});
-
     const getRandomUser = () => {
         const url = 'https://randomuser.me/api';
         fetch(url)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => console.log(data.results[0]))
+            .catch(err => console.error(err));
     }
 
     return (
