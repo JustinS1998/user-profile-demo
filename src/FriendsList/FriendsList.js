7cf7e33112e6abbb9b34dca076a1ff262d3f3522
Justin Seitz <justinseitz2016@gmail.com>
2021-06-27 15:19:25 -0700

    figuring out styling, cleaned up some code

--- src/FriendsList/FriendsList.js
+++ src/FriendsList/FriendsList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-//import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -9,20 +8,15 @@ import Card from 'react-bootstrap/Card';
 export function FriendsList(props) {
     const { friends } = props;
     return (
-        // <ListGroup>
-        //     {friends.map((element) => {
-        //         return <ListGroup.Item>{element.name.first}</ListGroup.Item>
-        //     })}
-        // </ListGroup>
-        <Container>
+        <Container style={{border:'solid black 1px'}}>
             <Row className='flex-nowrap'>
                 {friends.map((element) => {
                     return (
-                        <Col>
+                        <Col lg={4}>
                             <Card>
                                 <Card.Img src={element.picture.thumbnail}/>
                                 <Card.Body>
-                                    <Card.Title>{element.name.first} {element.name.last}</Card.Title>
+                                    <Card.Title style={{fontSize:'15px'}}>{element.name.first} {element.name.last}</Card.Title>
                                 </Card.Body>
                             </Card>
                         </Col>
