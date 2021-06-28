fd9b17dfd0554eacd664aaae7415387c883f666e
Justin Seitz <justinseitz2016@gmail.com>
2021-06-27 17:29:41 -0700

    added link to github

--- src/MyNavbar/MyNavbar.js
+++ src/MyNavbar/MyNavbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Nav } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 
 export function MyNavbar(props) {
+    const url = 'https://github.com/JustinS1998/user-profile-demo';
     return (
-        <Navbar bg='light' expand='sm'>
+        <Navbar bg='light'>
             <Navbar.Brand>User Profile Demo</Navbar.Brand>
+            <Nav>
+                <Nav.Link href={url}>Github</Nav.Link>
+            </Nav>
         </Navbar>
     );
 }
